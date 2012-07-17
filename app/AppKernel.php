e74af68ad86f39919b637bcf79e77541f66f62d1
Carlos Mendoza <inhack20@gmail.com>
2012-07-17 14:54:04 -0430

    Terminada declaracion de clases y relaciones

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,13 +17,14 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            //new FOS\UserBundle\FOSUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new Symfony\Bundle\DoctrineFixturesBundle\DoctrineFixturesBundle(),
+            new INHack20\UserBundle\INHack20UserBundle(),
             new INHack20\ControlDistribucionBundle\INHack20ControlDistribucionBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+            //$bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
---
+++ src/INHack20/ControlDistribucionBundle/DataFixtures/ORM/ControlDistribucionFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace INHack20\ControlDistribucionBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Description of ControlDistribucionFixtures
+ *
+ * @author inhack20
+ */
+class ControlDistribucionFixtures implements FixtureInterface {
+    
+    public function load(ObjectManager $manager) {
+        
+    }
+}
+
+?>
